305fb4186b6161d821b184c3af7923abca9a3c94
Dora Choi <hellodoradora7@gmail.com>
2025-05-21 21:43:23 +0900

    feat: Add UnitController scripts

--- Assets/Script/GameSceneDirector.cs
+++ Assets/Script/GameSceneDirector.cs
@@ -13,13 +13,31 @@ public class GameSceneDirector : MonoBehaviour
     public GameObject prefabCursor;
     GameObject[,] tiles;
     UnitController[,] units;
+
+    //ユニットのプレハブ（色ごと）
     public List<GameObject> prefabWhiteUnits;
     public List<GameObject> prefabBlackUnits;
 
-    // Start is called before the first frame update
+    // 1＝ポーン、２＝ルーク、3＝ナイト、4＝ビショップ、５＝クイーン、6＝キング
+    public int[,] unitType =
+    {
+        { 2,1,0,0,0,0,11,12 },
+        { 3,1,0,0,0,0,11,13 },
+        { 4,1,0,0,0,0,11,14 },
+        { 5,1,0,0,0,0,11,15 },
+        { 6,1,0,0,0,0,11,16 },
+        { 4,1,0,0,0,0,11,14 },
+        { 3,1,0,0,0,0,11,13 },
+        { 2,1,0,0,0,0,11,12 },
+    };
+
+
     void Start()
     {
         tiles = new GameObject[TILE_X, TILE_Y];
+        units = new UnitController[TILE_X, TILE_Y];
+
+
         for (int i = 0; i < TILE_X; i++)
         {
             for (int j = 0; j < TILE_Y; j++)
@@ -33,6 +51,25 @@ public class GameSceneDirector : MonoBehaviour
                 GameObject tile = Instantiate(prefabTile[idx], pos, Quaternion.identity);
 
                 tiles[i, j] = tile;
+
+                //　ユニット作成
+                int type = unitType[i, j] % 10;
+                int player = unitType[i, j] / 10;
+
+                GameObject prefab = getPrefabUnit(player, type);
+                GameObject unit = null;
+                UnitController ctrl = null;
+
+                if (null == prefab) continue;
+
+                pos.y += 1.5f;
+                unit = Instantiate(prefab);
+
+                ctrl = unit.GetComponent<UnitController>();
+                ctrl.SetUnit(player, (UnitController.TYPE)type, tile);
+
+                units[i, j] = ctrl;
+
             }
         }
     }
@@ -40,6 +77,17 @@ public class GameSceneDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    GameObject getPrefabUnit(int player, int type)
+    {
+        int idx = type - 1;
+        if (0 > idx) return null;
+
+        GameObject prefab = prefabWhiteUnits[idx];
+
+        if (1 == player) prefab = prefabBlackUnits[idx];
+        return prefab;
     }
 }
--- Assets/Script/UnitController.cs
+++ Assets/Script/UnitController.cs
@@ -1,18 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
+    // 
+    public int Player;
+
+    public TYPE Type;
+    public int ProgressTurnCount;
+    public Vector2Int Pos, OldPos;
+
+    public List<STATUS> Status;
+
+    public enum TYPE
+    {
+        NONE = -1,
+        PAWN = 1,
+        ROOK,
+        KNIGHT,
+        BISHOP,
+        QUEEN,
+        KING
+    }
+
+    public enum STATUS
+    {
+        NONE = -1,
+        QSIDE_CASTLING = 1,
+        KSIDE_CASTLING,
+        EN_PASSANT,
+        CHECK
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetUnit(int player, TYPE type, GameObject tile)
+    {
+        Player = player;
+        Type = type;
+        MoveUnit(tile);
+        ProgressTurnCount = -1;
+    }
+
+    public void SelectUnit(bool select = true)
+    {
+        Vector3 pos = transform.position;
+        pos.y += 2;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        if (!select)
+        {
+            pos.y = 1.35f;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+        transform.position = pos;
+    }
+
+    public void MoveUnit(GameObject tile)
+    {
+        SelectUnit(false);
+
+        Vector2Int idx = new Vector2Int(
+            (int)tile.transform.position.x + GameSceneDirector.TILE_X / 2,
+            (int)tile.transform.position.y + GameSceneDirector.TILE_Y / 2);
+
+        Vector3 pos = tile.transform.position;
+        pos.y = 1.35f;
+        transform.position = pos;
+
+        Status.Clear();
+
+        if (TYPE.PAWN == Type)
+        {
+            if (1 < Mathf.Abs(idx.y - Pos.y))
+            {
+                Status.Add(STATUS.EN_PASSANT);
+
+                int dir = -1;
+                if (1 == Player) dir = 1;
+
+
+                Pos.y = idx.y + dir;
+            }
+        }
+
+        if (TYPE.KING == Type)
+        {
+            if (1 < idx.x - Pos.x)
+            {
+                Status.Add(STATUS.KSIDE_CASTLING);
+            }
+
+            if (-1 > idx.x - Pos.x)
+            {
+                Status.Add(STATUS.QSIDE_CASTLING);
+            }
+        }
+
+        OldPos = Pos;
+        Pos = idx;
+
+        ProgressTurnCount = 0;
+    }
+
+    public void ProgressTurn()
+    {
+        if (0 > ProgressTurnCount) return;
+        ProgressTurnCount++;
+        if (TYPE.PAWN == Type)
+        {
+            if (1 < ProgressTurnCount)
+            {
+                Status.Remove(STATUS.EN_PASSANT);
+            }
+        }
+    }
+
+    UnitController getEnPassantUnit(UnitController[,] units, Vector2Int pos)
+    {
+        foreach (var v in units)
+        {
+            if (null == v) continue;
+            if (Player == v.Player) continue;
+            if (!v.Status.Contains(STATUS.EN_PASSANT)) continue;
+
+            if (v.OldPos == pos) return v;
+        }
+
+        return null;
+    }
+
+    public void SetCheckStatus(bool flag = true)
+    {
+        Status.Remove(STATUS.CHECK);
+        if (flag) Status.Add(STATUS.CHECK);
     }
 }
